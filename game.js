5d3a565417530bc7b451e91297b77239e68aecc7
Shun <155858568+S-Shun2960@users.noreply.github.com>
2024-04-27 15:44:50 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,8 @@
-// This is a JavaScript file
-
 var count = 0
 window.setInterval(countup, 1000);
 
 function countup(){
   count = count + 1
-  console.log(count)
   document.getElementById("count").innerHTML = count;
 }
 
